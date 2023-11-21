67a001c1ac50a1185bcd251d951a99213f69ab57
Serhii Kondiuk <serhii.kondiuk@edvantis.com>
2023-11-21 23:49:53 +0200

    Removed default app-component markup. Added initial header and conversion components functionalities.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ConversionComponent } from './conversion/conversion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ConversionComponent
   ],
   imports: [
     BrowserModule,
