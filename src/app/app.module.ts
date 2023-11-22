0502e571fbe290ba8f0f90bca942aa0fbe75fcb6
Serhii Kondiuk <serhii.kondiuk@edvantis.com>
2023-11-22 11:35:56 +0200

    Added initial data service functionality. Moved header and conversion components into components folder.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ConversionComponent } from './conversion/conversion.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ConversionComponent } from './components/conversion/conversion.component';
 
 @NgModule({
   declarations: [
