df4c49a98260e5d3e651651cd0e1913e2820cc78
Serhii Kondiuk <serhii.kondiuk@edvantis.com>
2023-11-22 12:02:42 +0200

    Updated header component initial functionality. Added API results displaying. Added initial API functionality based on cdn.jsdelivr.net.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  currency: string = '';
+  value: string = '';
 
+  apiResult: any;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.dataService.getCurrenciesRate().subscribe((data: any) => {
+      this.apiResult = data;
+    });
+  }
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+  readonly CURRENCIES_API = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur.json';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  public getCurrenciesRate(): Observable<any> {
+    return this.httpClient.get(this.CURRENCIES_API);
+  }
 }
